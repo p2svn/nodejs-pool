299ea2cbb7e9170377aea09e363ad5d9bfb70c1a
Pay 2 Services <33637233+p2svn@users.noreply.github.com>
2018-02-27 09:25:53 -0800

    Update deploy.bash

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -23,21 +23,22 @@ sudo make
 sudo mv libg* /usr/lib/
 cd ~
 sudo systemctl enable ntp
-#cd /usr/local/src
-#sudo git clone https://github.com/electroneum/electroneum monero
-#cd monero
-#sudo git checkout v0.11.1.0
-#curl https://raw.githubusercontent.com/Snipa22/nodejs-pool/master/deployment/monero_daemon.patch | sudo git apply -v
-#sudo make -j$(nproc)
-#sudo cp ~/nodejs-pool/deployment/monero.service /lib/systemd/system/
-#sudo useradd -m monerodaemon -d /home/monerodaemon
-#BLOCKCHAIN_DOWNLOAD_DIR=$(sudo -u monerodaemon mktemp -d)
-#sudo -u monerodaemon wget --limit-rate=50m -O $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw https://downloads.getmonero.org/blockchain.raw
-#sudo -u monerodaemon /usr/local/src/monero/build/release/bin/monero-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --batch-size 20000 --database lmdb#fastest --verify off --data-dir /home/monerodaemon/.bitmonero
-#sudo -u monerodaemon rm -rf $BLOCKCHAIN_DOWNLOAD_DIR
-#sudo systemctl daemon-reload
-#sudo systemctl enable monero
-#sudo systemctl start monero
+cd /usr/local/src
+sudo git clone https://github.com/electroneum/electroneum
+cd electroneum
+sudo git checkout v0.11.1.0
+curl https://raw.githubusercontent.com/p2svn/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
+sudo make -j$(nproc)
+sudo cp ~/nodejs-pool/deployment/electroneum.service /lib/systemd/system/
+sudo useradd -m electroneumdaemon -d /home/electroneumdaemon
+BLOCKCHAIN_DOWNLOAD_DIR=$(sudo -u electroneumdaemon mktemp -d)
+sudo -u electroneumdaemon wget --limit-rate=50m -O $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw http://61.14.237.206/blockchain.raw
+sudo -u electroneumdaemon mkdir data
+sudo -u electroneumdaemon /usr/local/src/electroneum/build/release/bin/electroneum-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --batch-size 20000 --database lmdb#fastest --verify off --data-dir /home/electroneumdaemon/data
+sudo -u electroneumdaemon rm -rf $BLOCKCHAIN_DOWNLOAD_DIR
+sudo systemctl daemon-reload
+sudo systemctl enable electroneum
+sudo systemctl start electroneum
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install v8.9.3
